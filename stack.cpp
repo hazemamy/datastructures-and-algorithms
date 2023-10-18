7a8bc3cf600b05366a4bea0f6fae139fc2640f11
hazem ahmed <99791497+hazemamy@users.noreply.github.com>
2023-10-18 23:17:08 +0300

    Valid Parentheses

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,47 @@
-#include<iostream>
+#include <unordered_map>
+#include <stack>
+#include <string>
+#include <iostream>
 using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+
+        unordered_map<char, char> Mmap = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}};
+        stack<char> myStack;
+
+        for (const auto &c : s)
+        {
+            if (Mmap.find(c) != Mmap.end())
+            {
+                if (myStack.empty())
+                {
+                    return false;
+                }
+                if (myStack.top() != Mmap[c])
+                {
+                    return false;
+                }
+                myStack.pop();
+            }
+            else
+            {
+                myStack.push(c);
+            }
+        }
+
+        return myStack.empty();
+    }
+};
+
 int main()
 {
-       
-    return 0;
+    Solution s;
+    cout << s.isValid("()[]{}") << endl;
 }
